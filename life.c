2b5fc53b9a55dcdf89e6612cda92694dc1fde7c9
Satoshi OKUI <okuisatoshi@mac.com>
2024-05-13 11:48:52 +0900

    穴埋め

--- life.c
+++ life.c
@@ -14,64 +14,28 @@ char world[I][J];
 
 void draw(int gen)
 {
-    for (int i = 0; i < I; i++) {
-        for (int j = 0; j < J; j++)
-            printf(world[i][j] ? "◽️" : "  ");
-        printf("\n");
-    }
-    printf("%6d世代\n", gen);
 }
 
 int count_neighbors(int i, int j)
 {
-    int n = 0;
-    for (int di = -1; di < 2; di++)
-        for (int dj = -1; dj < 2; dj++) {
-            if (di == 0 && dj == 0) continue; // 自分は数えない
-            n += world[(i + di + I) % I][(j + dj + J) % J];
-        }
-    return n;
 }
 
 void update(void)
 {
-    char next[I][J];
-    for (int i = 0; i < I; i++)
-        for (int j = 0; j < J; j++) {
-            const int c = count_neighbors(i, j);
-            next[i][j] = c == 2 ? world[i][j] : c == 3 ? 1 : 0;
-        }
-    memcpy(world, next, sizeof(world));
 }
 
 void init(void)
 {
-    srand(10); // 種10を変えると盤面の初期パターンが変わる
-    for (int i = 0; i < I; i++)
-        for (int j = 0; j < J; j++)
-            //world[i][j] = rand() * 2.0 / (RAND_MAX + 1.0);
-            world[i][j] = rand() % 2;
 }
 
 void clear(void)
 {
-    printf("\x1B[2J\x1B[H"); // 画面クリア&カーソルを先頭に
 }
 
 void wait(int ms)
 {
-    struct timespec rem, req = { 0, ms * 1000000 };
-    while(nanosleep(&req, &rem) == -1) req = rem;
 }
 
 int main()
 {
-    int gen = 0;
-    init();
-    for (;;) {
-        clear();
-        draw(gen++);
-        update();
-        wait(30); // 更新の間隔は適宜調整(ms)
-    }
 }
